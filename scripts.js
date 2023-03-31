af13ca9c868b14a942ad6cb311c6c740a0c3f6d6
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-03-30 19:16:07 -0700

    feat: Defind Size obj for winow dim. storage

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ function Size(width = 80, height = 60) {
   this.width = width;
   this.height = height;
 }
+
+Size.prototype.resize = function(newWidth, newHeight) {
+  this.width = newWidth;
+  this.height = newHeight;
+}
